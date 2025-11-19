1472396fbbdaa360fbbbe820d921d69483dd1d49
islomAli99 <islomaxmadjanov99@gmail.com>
2025-11-19 22:34:32 +0500

    Add missing Error view and localization package

--- obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827dbf828458ba762157e512ab79870f7162ed73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c999cf41296cd44f4b5cd31ac30d214b14da6c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
